881468bb899690487f716dc5f2552fe0d3352665
sg788 <sumangtm1083@gmail.com>
2024-12-18 11:14:51 +0630

    Weather KMM

---
+++ composeApp/src/commonMain/kotlin/org/example/weatherkmm/Model/CurrentUnits.kt
@@ -0,0 +1,12 @@
+package org.example.weatherkmm.Model
+
+data class CurrentUnits(
+    val cloud_cover: String,
+    val interval: String,
+    val is_day: String,
+    val temperature_2m: String,
+    val time: String,
+    val weather_code: String,
+    val wind_direction_10m: String,
+    val wind_speed_10m: String
+)

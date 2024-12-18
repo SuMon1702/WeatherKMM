87e5c02e7bf826486ce0a786aa29ba08f354ebf5
sg788 <sumangtm1083@gmail.com>
2024-12-18 11:15:24 +0630

    Weather KMM

---
+++ composeApp/src/commonMain/kotlin/org/example/weatherkmm/Model/DailyUnits.kt
@@ -0,0 +1,9 @@
+package org.example.weatherkmm.Model
+
+data class DailyUnits(
+    val sunrise: String,
+    val sunset: String,
+    val temperature_2m_max: String,
+    val temperature_2m_min: String,
+    val time: String
+)
